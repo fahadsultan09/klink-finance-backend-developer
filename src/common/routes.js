176732f0618a84d12a82dbb5f4e2630f3f3ed6f8
Fahad Sultan <fahadsultan1998@gmail.com>
2022-09-02 16:16:49 +0500

    modification : GET route fixed

--- src/common/routes.js
+++ src/common/routes.js
@@ -1,7 +1,7 @@
+var express = require('express');
+var router = express.Router();
 const employeeController = require("../controllers/employeeController.js");
-module.exports = app => {
-    app.get("/employee", employeeController.get);
-    app.post("/employee", employeeController.create);
-    app.put("/employee/:id", employeeController.update);
-    app.delete("/employee/:id", employeeController.delete);
-};
+
+router.route('/employee').get(employeeController.get);
+
+module.exports = router;
